00762b415f97fe7d595e30d81cf5415b4c54806e
Lazuardi Pratama Putra Nusantara <ardinusantara07@gmail.com>
2025-05-07 21:28:03 +0700

    add all models to admin page

--- barang/admin.py
+++ barang/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Get all model classes from models.py
+model_classes = [model for model in vars().values() if isinstance(model, type) and issubclass(model, models.Model)]
+
+# Register all model classes to the admin site
+for model_class in model_classes:
+    if not model_class._meta.abstract:
+        try:
+            admin.site.register(model_class)
+        except admin.sites.AlreadyRegistered:
+            pass
